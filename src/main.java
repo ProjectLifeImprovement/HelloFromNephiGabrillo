1c6698540ba7683e87c41182051395053010e5d6
Yareh <cha15080@byui.edu>
2021-06-16 15:47:31 -0600

    Changes made by Minhah

--- src/main.java
+++ src/main.java
@@ -1,5 +1,7 @@
+
 public class main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("Hello, my name is Kyerra Bright");
+        System.out.println("Hi Kyerra, my name is Minhah");
     }
 }
